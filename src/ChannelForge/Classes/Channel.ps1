731cd99ba302bc4ae29b591a873ae7c666db46a7
Eli <EliSiegel@gmail.com>
2026-06-26 23:02:44 -0400

    feat(domain): add Channel domain object

---
+++ src/ChannelForge/Classes/Channel.ps1
@@ -0,0 +1,42 @@
+BeforeAll {
+    $RepoRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
+    Import-Module (Join-Path $RepoRoot 'src\ChannelForge\ChannelForge.psd1') -Force
+}
+
+Describe 'New-ChannelForgeChannel' {
+    It 'creates a channel with basic identity fields' {
+        $channel = New-ChannelForgeChannel `
+            -Provider 'mybunny' `
+            -Playlist 'Sports' `
+            -OriginalName 'ESPN HD' `
+            -TvgId 'espn.us' `
+            -Group 'Sports'
+
+        $channel.Provider | Should -Be 'mybunny'
+        $channel.Playlist | Should -Be 'Sports'
+        $channel.OriginalName | Should -Be 'ESPN HD'
+        $channel.NormalizedName | Should -Be 'ESPN HD'
+        $channel.DisplayName | Should -Be 'ESPN HD'
+        $channel.TvgId | Should -Be 'espn.us'
+        $channel.Group | Should -Be 'Sports'
+    }
+
+    It 'uses DisplayName when provided' {
+        $channel = New-ChannelForgeChannel `
+            -OriginalName 'WABC HD' `
+            -DisplayName 'WABC ABC New York'
+
+        $channel.DisplayName | Should -Be 'WABC ABC New York'
+    }
+
+    It 'initializes classification and flags safely' {
+        $channel = New-ChannelForgeChannel -OriginalName 'Test Channel'
+
+        $channel.Category | Should -Be ''
+        $channel.IsLocal | Should -BeFalse
+        $channel.IsAdult | Should -BeFalse
+        $channel.IsDuplicate | Should -BeFalse
+        $channel.Confidence | Should -Be 0
+        $channel.Warnings.Count | Should -Be 0
+    }
+}
