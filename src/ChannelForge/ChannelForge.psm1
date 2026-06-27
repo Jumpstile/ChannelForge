5d9020ba8d09f64d5c7f790fe595ea59706a16d4
Eli <EliSiegel@gmail.com>
2026-06-26 22:04:39 -0400

    refactor(module): add ChannelForge module skeleton

---
+++ src/ChannelForge/ChannelForge.psm1
@@ -0,0 +1,13 @@
+$Public = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
+$Private = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
+
+foreach ($import in @($Private + $Public)) {
+    try {
+        . $import.FullName
+    }
+    catch {
+        throw "Failed to import function $($import.FullName): $_"
+    }
+}
+
+Export-ModuleMember -Function $Public.BaseName
